f53cb2e3c0e6ca741cb0c54949114e987c892cd7
sjbatista <salomaojunio@hotmail.com>
2022-09-27 12:25:09 -0300

    We use the 'webView' component, we write html code inside the 'webview'

--- App.js
+++ App.js
@@ -6,7 +6,43 @@ export default function App() {
   return (
     <WebView 
       style={styles.container}
-      source={{ uri: 'https://google.com' }}
+      source={{ html: `
+      <html>
+
+        <head>
+          <meta name= "viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        </head>
+
+        <body>
+      
+        <style>
+          *{
+            margin:0;
+            padding:0;
+            box-sizing:border-box;
+          }
+          .btn{
+            width:100%;
+            height:20px;
+            background-color:#069;
+            text-align:center;
+            color:white;
+          }
+        </style>
+        
+        <div id="btn" class="btn">Clique here !</div>
+
+        <script>
+          var el = document.getElementById('btn');
+          el.addEventListener('click', ()=>{
+            alert('click !');
+          });
+        </script>
+        </body>
+
+      </html> 
+    
+      ` }}
     />
   );
 }
